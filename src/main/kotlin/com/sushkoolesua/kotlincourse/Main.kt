7212f21c91df882fdb7df1f0235af684de69baab
Сушко Олеся Владимировна <OVVorobyeeva@alfabank.ru>
2024-09-15 15:57:53 +0700

    Homework for 3 lesson

--- src/main/kotlin/com/sushkoolesua/kotlincourse/Main.kt
+++ src/main/kotlin/com/sushkoolesua/kotlincourse/Main.kt
@@ -1,4 +1,4 @@
-package com.sushkoolesua
+package com.sushkoolesua.com.sushkoolesua.kotlincourse
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
