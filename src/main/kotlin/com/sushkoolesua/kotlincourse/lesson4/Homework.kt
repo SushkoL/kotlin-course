7ca076e9d3a5b07877ff957e96ac6e74ec547982
Сушко Олеся Владимировна <OVVorobyeeva@alfabank.ru>
2024-09-15 16:58:03 +0700

    Homework for 4 lesson

---
+++ src/main/kotlin/com/sushkoolesua/kotlincourse/lesson4/Homework.kt
@@ -0,0 +1,83 @@
+package com.sushkoolesua.kotlincourse.lesson4
+
+//Для каждого значения допиши подходящий тип данных
+
+Int = 42
+
+Long = 98765432123456789L
+
+Float = 23.45f
+
+Double = 0.123456789
+
+String = "Kotlin & Java"
+
+Boolean = "false"
+
+Char = 'c'
+
+Int = 500
+
+Long = 4294967296L
+
+Float = 18.0f
+
+Double = -0.001
+
+String = "OpenAI"
+
+Boolean = "true"
+
+Char = '9'
+
+Int = 2048
+
+Long = 10000000000L
+
+Float = 5.75f
+
+Double = 1.414
+
+String = "Artificial Intelligence"
+
+Boolean = "false"
+
+Char = '@'
+
+Int = 1024
+
+Long = 1234567890123L
+
+Float = 10.01f
+
+Double = -273.15
+
+String = "SpaceX"
+
+Boolean = "true"
+
+Any = "🤯"
+
+Int = 65535
+
+Long = 72057594037927935L
+
+Float = 2.71828f
+
+Double = 101.0101
+
+String = "Quantum Computing"
+
+Boolean = "false"
+
+Char = 'x'
+
+Int = 314
+
+Long = 123456789123456789L
+
+Float = 6.626f
+
+Double = 0.007
+
+String = "Android Studio"
